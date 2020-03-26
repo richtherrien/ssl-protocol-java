ed182e2108cf2fe40f491ed432267097065c25f1
Richard Therrien <richardgtherrien@gmail.com>
2020-03-26 12:42:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ssl-protocol-java
---
+++ ssl-project/src/project/Project.java
@@ -0,0 +1,15 @@
+package project;
+
+/**
+ *
+ * @author 
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+}
