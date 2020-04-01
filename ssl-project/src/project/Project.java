d8c7257d31dfb6e2f959e85621166f52c1833cf2
Richard Therrien <richardgtherrien@gmail.com>
2020-04-01 14:44:01 -0400

    dev-9: Added initial connection between server and client

--- ssl-project/src/project/Project.java
+++ ssl-project/src/project/Project.java
@@ -1,8 +1,11 @@
 package project;
 
+import client.Client;
+import server.Server;
+
 /**
  *
- * @author 
+ * @author
  */
 public class Project {
 
@@ -10,6 +13,14 @@ public class Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        if (args[0].equals("client")) {
+            Client client = new Client();
+            client.main();
+        } else if (args[0].equals("server")) {
+            Server server = new Server();
+            server.main();
+        } else {
+            System.out.println("unrecognized argument\nPlease give an arugment of: server or client");
+        }
     }
 }
---
+++ ssl-project/src/server/ServerRunnable.java
@@ -0,0 +1,40 @@
+package server;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Richard
+ */
+public class ServerRunnable implements Runnable {
+    private final Socket socket;
+    public ServerRunnable(Socket socket){
+        System.out.println("Creating a runnable thread of the Server");
+        this.socket = socket;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            DataInputStream in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        } catch (IOException ex) {
+            Logger.getLogger(ServerRunnable.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+            socket.close();
+            System.out.println("Closed Socket");
+
+            } catch (IOException ex) {
+                Logger.getLogger(ServerRunnable.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
