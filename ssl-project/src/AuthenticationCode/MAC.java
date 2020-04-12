558ac3db6864a468b7ceb8ecf1b2ce5a5ce7fd5c
Roman M <rmakuch@ryerson.ca>
2020-04-11 20:01:03 -0400

    dev-7: Added MD5 and SHA alogrithms

--- ssl-project/src/AuthenticationCode/MAC.java
+++ ssl-project/src/AuthenticationCode/MAC.java
@@ -1,38 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package AuthenticationCode;
+
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.Mac;
-/**
- *
- * @author Roman Makuch
- */
+
 public class MAC {
-    public MAC(Key key, String msg) {  
+     protected byte[] MAC(Key key, String msg, String algorithm) {
         Mac mac = null;
-        try {
-            //Creating MAC
-            mac = Mac.getInstance("MD5");
-            //Initialize Mac Object
-            mac.init(key);
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(MAC.class.getName()).log(Level.SEVERE, null, ex);
+        if ("MD5".equals(algorithm) || "SHA-1".equals(algorithm)){
+         
+            try {
+                mac = Mac.getInstance(algorithm);
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(MAC.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        catch (InvalidKeyException ex) {
-            Logger.getLogger(MAC.class.getName()).log(Level.SEVERE, null, ex);
+        //Default to MD5
+        else{
+            System.out.println("Defaulted to MD5");
+            try {
+                mac = Mac.getInstance("MD5");
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(MAC.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        
+        //Initializing the Mac
+         try {
+             mac.init(key);
+         } catch (InvalidKeyException ex) {
+             Logger.getLogger(MAC.class.getName()).log(Level.SEVERE, null, ex);
+         }
         //Computing the Mac
         byte[] bytes = msg.getBytes();      
         byte[] macResult = mac.doFinal(bytes);
+        System.out.println(algorithm);
         System.out.println("Mac result:");
         System.out.println(new String(macResult));
-   }
+        return macResult;
+    }
 }
